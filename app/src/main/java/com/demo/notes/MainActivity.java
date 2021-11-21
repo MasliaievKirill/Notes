b2514f4c0dbd6fb664a99fe9751135a5077aaf1f
MasliaievKirill <kirillmaslyaev@gmail.com>
2021-11-21 21:35:24 +0200

    1.Add NotesAdapter.

--- app/src/main/java/com/demo/notes/MainActivity.java
+++ app/src/main/java/com/demo/notes/MainActivity.java
@@ -1,14 +1,32 @@
 package com.demo.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewNotes;
+    private ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        NotesAdapter adapter = new NotesAdapter(notes);
+        recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewNotes.setAdapter(adapter);
     }
 }
