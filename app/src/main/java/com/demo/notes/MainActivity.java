4b8bb43df6cdcc7f5c838d1977cb70ca3fc0e6dc
MasliaievKirill <kirillmaslyaev@gmail.com>
2021-11-22 22:01:40 +0200

    1.Add AddNoteActivity. 1.Add onClick, onSwipe.

--- app/src/main/java/com/demo/notes/MainActivity.java
+++ app/src/main/java/com/demo/notes/MainActivity.java
@@ -1,32 +1,73 @@
 package com.demo.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewNotes;
-    private ArrayList<Note> notes = new ArrayList<>();
+    public static final ArrayList<Note> notes = new ArrayList<>();
+    private NotesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        notes.add(new Note("Заголовок", "Описание", "Day", 2));
-        NotesAdapter adapter = new NotesAdapter(notes);
+        if (notes.isEmpty()) {
+            notes.add(new Note("Заголовок", "Описание", "Day", 2));
+            notes.add(new Note("Заголовок", "Описание", "Day", 1));
+            notes.add(new Note("Заголовок", "Описание", "Day", 3));
+            notes.add(new Note("Заголовок", "Описание", "Day", 2));
+            notes.add(new Note("Заголовок", "Описание", "Day", 1));
+            notes.add(new Note("Заголовок", "Описание", "Day", 3));
+            notes.add(new Note("Заголовок", "Описание", "Day", 2));
+        }
+        adapter = new NotesAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
+        adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClick(int position) {
+                Toast.makeText(MainActivity.this, "Нажат", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(int position) {
+                remove(position);
+            }
+        });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                remove(viewHolder.getAdapterPosition());
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
+    }
+
+    private void remove (int position) {
+        notes.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void onClickAddNote(View view) {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
     }
 }
